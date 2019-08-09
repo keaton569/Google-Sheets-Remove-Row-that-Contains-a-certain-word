bc4f20a14713e29ac80cca0decde6439b8fc8b3c
keaton569 <keatonguitar123@gmail.com>
2019-08-09 13:07:44 -0400

    Update remove.js

--- remove.js
+++ remove.js
@@ -3,10 +3,17 @@ function myFunction() {
   var lastRow = Sheet1.getLastRow();
   
   for( var i = 2; i < lastRow; i++){
-    //var value = Sheet1.getValue(i, 4);
-    if(!Sheet1.getRange(i, 4).getValue().match("DNU")){
+    
+    //IF the cell does not contain the item you are searching for
+    // Then remove it
+    //remember to remove the {}'s too
+    if(!Sheet1.getRange(i, {column To Search}).getValue().match("{item to look for}")){
+      
+      //Lets you know how far along the code is. Super helpful on sheets bigger than 5k rows.
       SpreadsheetApp.getActiveSpreadsheet().toast(i);
-      Sheet1.deleteRow(i)
+      
+      Sheet1.deleteRow(i);
+      //reset row on row delete;
       i--;
     }
   }
